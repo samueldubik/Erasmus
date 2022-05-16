756bc58a6d388fcadff0f223a12f022537331b20
Samuel Dubik <samuel.dubik@gmail.com>
2022-05-16 22:33:40 +0200

    Mobile version work in progress

--- erasmus-frontend/src/App.vue
+++ erasmus-frontend/src/App.vue
@@ -3,14 +3,14 @@
 <template>
   <div>
     
-    <header class="">
+    <header class=" mb-5">
       <div class=" color-decoration h-4 mb-10"></div>
       <h1 class=" text-3xl font-press text-center">Erasmus+</h1>
       <h2 class=" text-2xl font-source text-center mt-3">Home</h2>
       <nav class=" hidden">asdsad</nav>
     </header>
 
-    <article class="mx-6 mt-2">
+    <article class="mx-3.5 mt-2">
       <section>
         
         <div class=" bg-light pl-4 pr-8 pt-7 rounded-md">
@@ -18,16 +18,17 @@
           <hr class=" border-2 border-gray">
           <p class=" font-source mt-5">Yes, that's a good idea to eplore and see the world.
              Look at the available otions where you could go</p>
-          <button class=" bg-blue w-1/2 h-12 text-white font-press mt-5 rounded-md mb-10">apply ></button>
+          <button class=" bg-blue w-2/3 h-12 text-white font-press mt-12 rounded-md mb-10 custom-button-blue">apply ></button>
         </div>
 
-        <div>
-          <h2>I just arrived</h2>
-          <p class="">First of all, welcome!
+        <div class=" bg-light pl-4 pr-8 pt-7 rounded-md mt-3">
+          <h2 class=" text-xl font-press  text-green">I just arrived</h2>
+          <hr class=" border-2 border-gray">
+          <p class=" font-source mt-5">First of all, welcome!
             We're glad that you are here. I guess
             you're interested in what's next.
           </p>
-          <button>continue ></button>
+          <button class=" bg-lime-green w-2/3 h-12 text-white font-press mt-12 rounded-md mb-10 custom-button-blue">continue ></button>
         </div>
 
       </section>
@@ -39,24 +40,24 @@
 
       </section>
 
-      <section>
+      <section class="mt-10">
 
-        <div>
-          <h2>FAQ</h2>
-          <p>How, what, deadlines, ...</p>
-          <button>go ?</button>
+        <div class=" bg-light pl-4 pr-8 pt-7 rounded-md mt-3">
+          <h2 class=" text-xl font-press  text-center">FAQ</h2>
+          <p class=" font-source mt-5 text-center">How, what, deadlines, ...</p>
+          <button class=" bg-yellow ml-[15%] w-2/3 h-12 text-white font-press mt-12 rounded-md mb-10 custom-button-blue">go ?</button>
         </div>
 
-        <div>
-          <h2>Feedback</h2>
-          <p>What you liked or disliked</p>
-          <button>submit *</button>
+        <div class=" bg-light pl-4 pr-8 pt-7 rounded-md mt-3">
+          <h2 class=" text-xl font-press  text-center ">Feedback</h2>
+          <p class=" font-source mt-5 text-center">What you liked or disliked</p>
+          <button class=" bg-pink ml-[15%] w-2/3 h-12 text-white font-press mt-12 rounded-md mb-10 custom-button-blue">submit *</button>
         </div>
 
-        <div>
-          <h2>Offered courses</h2>
-          <p>List of courses that we have</p>
-          <button>list #</button>
+        <div class=" bg-light pl-4 pr-8 pt-7 rounded-md mt-3">
+          <h2 class=" text-xl font-press  text-center">Offered courses</h2>
+          <p class=" font-source mt-5 text-center">List of courses that we have</p>
+          <button class=" bg-green ml-[15%] w-2/3 h-12 text-white font-press mt-12 rounded-md mb-10 custom-button-blue">list #</button>
         </div>
 
       </section>
